4515987ed1fb4889a3ee2c7c07c3d14994c6697e
ue-dnd <12403010122@stu.cqut.edu.cn>
2024-10-28 11:27:24 +0800

    update files

--- experiment/4.4/4.4.2/4.4.2.5/main.cpp
+++ experiment/4.4/4.4.2/4.4.2.5/main.cpp
@@ -2,10 +2,12 @@
 
 int main() {
     int i, j;
-
     for (i = 1; i <= 9; i++) {
-        for (j = 1; j <= i; j++) {
-            printf("%d*%d=%2d  ", j, i, j * i);
+        for (j = 1; j < i; j++) {
+            printf("           ");
+        }
+        for (j = i; j <= 9; j++) {
+            printf("%d*%d=%-5d  ", i, j, i * j);
         }
         printf("\n");
     }
