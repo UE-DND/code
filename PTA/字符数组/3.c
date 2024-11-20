5eb97f594f7720a2dd42ce01360649b6f577a736
UE-DND <12403010122@stu.cqut.edu.cn>
2024-11-20 21:33:51 +0800

    update files

--- PTA/字符数组/3.c
+++ PTA/字符数组/3.c
@@ -19,3 +19,91 @@
 
 ***********************************/
 
+#include <stdio.h>
+#include <string.h>
+
+char *num_to_chinese(int num) {
+    char *digit[] = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+    return digit[num];
+}
+
+void zhengshu_part(int num, char *result) {
+    char *danwei[] = {"", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿"};  // 第一个""实际上用不到，只是为了省去映射步骤
+    int unit = 0;  // 位数
+    int zero= 0;  // 标记是否需要输出"零"
+
+    if (num == 0)
+    {
+        strcat(result, "零");  // strcat的功能是将两个数组合并
+        return;
+    }
+    
+    while (num > 0) {
+        int digit = num % 10;
+        if (digit != 0)
+        {
+            if (zero)
+            {
+                strcat(result, "零");
+                zero = 0;
+            }
+            strcat(result, num_to_chinese(digit));  // 转换数字
+            strcat(result, danwei[unit]);  // 添加单位
+        } else {
+            zero = 0;  // 遇到0，标记，待后面补充“零”
+        }
+
+        num /= 10;
+        unit++;  // 每次单位向左移动
+    }
+
+    // 逆转字符串，最终结果的顺序是从高位到低位
+    // （处理后结果需要从最后一位的单位向前进行拼接）
+    for (int i = 0; i < strlen(result); i++) {
+        char temp = result[i];
+        result[i] = result[strlen(result) - 1 - i];
+        result[strlen(result) - 1 - i] = temp;
+    }
+
+        strcat(result, "元");  // 将"元"添加到 result 的末尾
+}
+
+void xiaoshu_part(int num, char *result) {
+    // 如果没有小数，直接退出
+    if (num == 0) {
+        return;
+    }
+
+    int jiao = num / 10, fen = num % 10;  // 提取角和分
+
+    if (jiao != 0) {
+        strcat(result, num_to_chinese(jiao));  // 转换数字
+        strcat(result, "角");  //添加单位
+    }
+    if (fen != 0) {
+        strcat(result, num_to_chinese(fen));
+        strcat(result, "分");
+    }
+}
+
+int main(void) {
+    double input;
+    scanf("%lf", &input);
+
+    // 处理等会要进入函数的数字
+    int zhengshu = (int)input;
+    int xiaoshu = (int)((input - zhengshu) * 100);
+
+    char result[100] = {0};
+
+    zhengshu_part(zhengshu, result);
+    xiaoshu_part(xiaoshu, result);
+
+    // 去掉没有角和分的情况
+    if (xiaoshu == 0) {
+        result[strlen(result) - 1] = "\0";
+    }
+
+    printf("%s", result);
+    return 0;
+}
--- PTA/字符数组/5.c
+++ PTA/字符数组/5.c
@@ -1,39 +1,58 @@
 /**********************************
 
-从键盘输入一行字符，然后将其中所有的小写字母转换为对应的大写字母，
-将所有的大写字母转换为对应的小写字母，
-将所有的数字字符转换为ASCII代码表中的下一个字符，其他字符保持不变。
-
+给定两个长度分别为N和M的字符串A和B，求既是A的子序列又是B的子序列的字符串长度最长是多少。
 
 输入格式:
-一行字符。
+第一行包含两个整数N和M。1≤N,M≤1000
+
+第二行包含一个长度为N的字符串，表示字符串A。
+
+第三行包含一个长度为M的字符串，表示字符串B。
+
+字符串均由小写字母构成。
 
 输出格式:
-变换之后的字符串，最后换行。
+输出一个整数，表示最大长度。
 
 输入样例:
-HEllo56789#$&%?
+4 5
+acbd
+abcbd
 
 输出样例:
-heLLO6789:#$&%?
+4
 
 ***********************************/
 
-#include<stdio.h>
-#include<string.h>
-int main() {
-    char str[100];
-    gets(str);
-    for(int i = 0; i < strlen(str); i++) {
-        if(str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str[i] - 'a' + 'A';
-        } else if(str[i] >= 'A' && str[i] <= 'Z') {
-            str[i] = str[i] - 'A' + 'a';
-            } else if(str[i] >= '0' && str[i] <= '9') {
-                str[i] = str[i] + 1;
-            }
-        }
+// 这个方法不能通过部分测试点，因为时间复杂度太高，懒得改了
+// 感兴趣可以研究一下动态规划，我看不懂。
+#include <stdio.h>
+#include <string.h>
 
-    puts(str);
+int function(int i, int j, char *str1, char *str2) {
+    if (i == 0 || j == 0) {
+        return 0;
+    }
+
+    // 如果当前字符相同，处理前一个字符
+    if (str1[i - 1] == str2[j - 1])
+    {
+        return function(i - 1, j - 1, str1, str2) + 1;  //长度加一
+    } else {
+        // 否则，取去掉其中一个字符的结果中的较大值
+        return (function(i - 1, j, str1, str2) > function(i, j - 1, str1, str2)) ?
+                function(i - 1, j, str1, str2) : function(i, j - 1, str1, str2);
+    }
+
+}
+
+int main(void) {
+    char str1[1000], str2[1000];
+    int N, M;
+    scanf("%d %d", &N, &M);
+    scanf("%s", str1);
+    scanf("%s", str2);
+    int len = function(N, M, str1, str2);
+    printf("%d", len);
     return 0;
 }
