1e4488b67d10f37fff8262933cb4a0067ae1c358
UE-DND <12403010122@stu.cqut.edu.cn>
2024-11-21 17:52:52 +0800

    update files

--- PTA/字符数组/3.c
+++ PTA/字符数组/3.c
@@ -20,90 +20,47 @@
 ***********************************/
 
 #include <stdio.h>
-#include <string.h>
 
-char *num_to_chinese(int num) {
-    char *digit[] = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
-    return digit[num];
-}
+int main() {
 
-void zhengshu_part(int num, char *result) {
-    char *danwei[] = {"", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿"};  // 第一个""实际上用不到，只是为了省去映射步骤
-    int unit = 0;  // 位数
-    int zero= 0;  // 标记是否需要输出"零"
+    char *chinese[] = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+    char *danwei[] = {"元", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿"};
+    double num;
 
-    if (num == 0)
-    {
-        strcat(result, "零");  // strcat的功能是将两个数组合并
-        return;
-    }
-    
-    while (num > 0) {
-        int digit = num % 10;
-        if (digit != 0)
-        {
-            if (zero)
-            {
-                strcat(result, "零");
-                zero = 0;
-            }
-            strcat(result, num_to_chinese(digit));  // 转换数字
-            strcat(result, danwei[unit]);  // 添加单位
-        } else {
-            zero = 0;  // 遇到0，标记，待后面补充“零”
+    scanf("%lf", &num);
+
+    int zhengshu_part = (int)num;
+    int xiaoshu_part = (int)((num - zhengshu_part + 0.005) * 100);  // 0.005是一个极小值，为了防止浮点数舍入误差
+
+    int divisor = 10000000;
+    // 提取整数部分的每一位，并输出对应的汉字和单位
+    for (int i = 0; i < 8; i++) {
+        int digit = zhengshu_part / divisor;
+        if (digit != 0) {
+            printf("%s%s", chinese[digit], danwei[7-i]);
         }
-
-        num /= 10;
-        unit++;  // 每次单位向左移动
+        zhengshu_part %= divisor;
+        divisor /= 10;
     }
 
-    // 逆转字符串，最终结果的顺序是从高位到低位
-    // （处理后结果需要从最后一位的单位向前进行拼接）
-    for (int i = 0; i < strlen(result); i++) {
-        char temp = result[i];
-        result[i] = result[strlen(result) - 1 - i];
-        result[strlen(result) - 1 - i] = temp;
+    // 整数部分为0的特殊情况
+    if (zhengshu_part == 0) {
+        printf("元");
     }
 
-        strcat(result, "元");  // 将"元"添加到 result 的末尾
-}
-
-void xiaoshu_part(int num, char *result) {
-    // 如果没有小数，直接退出
-    if (num == 0) {
-        return;
+    if (xiaoshu_part != 0) {
+        int jiao = xiaoshu_part / 10;
+        int fen = xiaoshu_part % 10;
+        if(jiao > 0 && fen > 0) {
+            printf("%s%s%s%s", chinese[jiao], "角", chinese[fen], "分");
+        }
+        if(jiao > 0 && fen == 0) {
+            printf("%s%s", chinese[jiao], "角");
+        }
+        if(jiao == 0 && fen == 0);
+        if(jiao == 0 && fen > 0) {
+            printf("%s%s",chinese[fen], "分");
+        }
     }
-
-    int jiao = num / 10, fen = num % 10;  // 提取角和分
-
-    if (jiao != 0) {
-        strcat(result, num_to_chinese(jiao));  // 转换数字
-        strcat(result, "角");  //添加单位
-    }
-    if (fen != 0) {
-        strcat(result, num_to_chinese(fen));
-        strcat(result, "分");
-    }
-}
-
-int main(void) {
-    double input;
-    scanf("%lf", &input);
-
-    // 处理等会要进入函数的数字
-    int zhengshu = (int)input;
-    int xiaoshu = (int)((input - zhengshu) * 100);
-
-    char result[100] = {0};
-
-    zhengshu_part(zhengshu, result);
-    xiaoshu_part(xiaoshu, result);
-
-    // 去掉没有角和分的情况
-    if (xiaoshu == 0) {
-        result[strlen(result) - 1] = "\0";
-    }
-
-    printf("%s", result);
     return 0;
 }
