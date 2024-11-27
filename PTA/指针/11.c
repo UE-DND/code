c1b2a4f749097b64e0ea918395d6a8618afedc6f
ue-dnd <12403010122@stu.cqut.edu.cn>
2024-11-27 22:02:39 +0800

    update files

---
+++ PTA/指针/11.c
@@ -0,0 +1,42 @@
+/**********************************
+
+请使用指针的方法编写程序，程序的功能是从键盘输入10个数，求其最大值和最小值的差。
+
+输入格式:
+输入10个整数，每个整数之间用空格分隔。
+
+输出格式:
+同样例。
+
+输入样例:
+1 2 3 4 5 6 7 8 9 10
+
+输出样例:
+difference value = 9
+
+***********************************/
+
+#include <stdio.h>
+
+int main() {
+    int *num[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", num + i);
+    }
+
+    int max = *num[0], min = *num[0];
+    for (int i = 0; i < 10; i++) 
+    {
+        if (*(num + i) > max)
+        {
+            max = *(num + i);
+        }
+        if (*(num + i) < min)
+        {
+            min = *(num + i);
+        }
+    }
+    printf("difference value = %d", max - min);
+}
